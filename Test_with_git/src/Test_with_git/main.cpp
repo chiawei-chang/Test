91daa25aed455604a9e5bcd3588c56e13e42f586
chiawei-chang <legendgison@gmail.com>
2020-06-10 17:14:22 +0800

    1.[modify] Update 1.

--- Test_with_git/src/Test_with_git/main.cpp
+++ Test_with_git/src/Test_with_git/main.cpp
@@ -4,6 +4,7 @@ int main(int argc, char** argv)
 {
     std::cout << "Hello World!" << std::endl;
 
+    std::cout << "Update 1. 2020.06.10 " << std::endl;
 
     std::cout << "Goodbye!" << std::endl;
     return 0;
